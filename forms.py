9ee0a397e8f5836a55be496536cd91ca49e285b2
Your Name <you@example.com>
2021-03-18 11:59:15 +0100

    email validator

--- forms.py
+++ forms.py
@@ -1,15 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 
 class RegisterForm(FlaskForm):
-    email = StringField("User Email", [DataRequired()])
+    email = StringField("User Email", [DataRequired(), Email()])
     username = StringField("User Name",[DataRequired()])
     password = PasswordField("Password", [DataRequired()])
     submit = SubmitField("Sign In")
 
 
 class LoginForm(FlaskForm):
-    email = StringField("User Email", [DataRequired()])
+    email = StringField("User Email", [DataRequired(), Email()])
     password = PasswordField("Password", [DataRequired()])
     submit = SubmitField('Log In')
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, redirect, url_for, flash, request
 import flask
+from flask import Flask, render_template, redirect, url_for, flash, request
 from flask_bootstrap import Bootstrap
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
